654d0c2b61293d608a5740c1efd04e74a4a4eefa
Arijeet2514 <bunty.arijeet04@gmail.com>
2020-05-16 11:52:52 +0530

    Created template for c++

---
+++ Template_C++.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> p;
+typedef vector<int> v;
+typedef vector<p> vp;
+typedef vector<v> vv;
+
+void solve(){
+    // Your code goes here
+}
+
+int main(){
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0; 
+}
