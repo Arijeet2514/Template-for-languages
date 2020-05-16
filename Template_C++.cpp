604827e56e90248742fbf1e87db5c35a956e62ff
Arijeet2514 <bunty.arijeet04@gmail.com>
2020-05-16 15:40:47 +0530

    Added initialize and const and some more typedef statements

--- Template_C++.cpp
+++ Template_C++.cpp
@@ -1,10 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N=1e4+5;
+typedef long long ll;
 typedef pair<int,int> p;
 typedef vector<int> v;
 typedef vector<p> vp;
 typedef vector<v> vv;
+typedef pair<ll,p> pllp;
+typedef vector<pllp> vpllp;
+
+void initialize(){
+    for(int i=0;i<N;i++){
+        //Initializing an array with some initial value
+    }     
+}
 
 void solve(){
     // Your code goes here
